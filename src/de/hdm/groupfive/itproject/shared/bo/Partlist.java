30bcac4b35934ada44fc790200ed0cf2d8411490
Thomas Burkart <tb085@hdm-stuttgart.de>
2015-04-19 12:27:58 +0200

    Änderung an Klassendiagrammen

--- src/de/hdm/groupfive/itproject/shared/bo/Partlist.java
+++ src/de/hdm/groupfive/itproject/shared/bo/Partlist.java
@@ -1,9 +1,21 @@
 package de.hdm.groupfive.itproject.shared.bo;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Partlist extends HashMap<Element,Integer> {
+public class Partlist extends HashMap<Element,Integer> implements Serializable {
 	
-	
+	/**
+	 * 
+	 * @param element
+	 * @return
+	 */
+	public int getQuantityByElement(Element element) {
+		int result = -1;
+		if (this.containsKey(element)) {
+			result = this.get(element);
+		}
+		return result;
+	}
 
 }
