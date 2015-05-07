8acc8bbaf38884e8a905b2de806ee0a80b935d42
TimoFesseler <timo.fesseler@gmail.com>
2015-05-07 09:40:27 +0200

    Interface IAdministrationAsync hinzugefügt

--- src/de/hdm/groupfive/itproject/shared/IAdministrationCommonAsync.java
+++ src/de/hdm/groupfive/itproject/shared/IAdministrationCommonAsync.java
@@ -1,5 +1,16 @@
 package de.hdm.groupfive.itproject.shared;
 
-public interface IAdministrationCommonAsync {
+import de.hdm.groupfive.itproject.shared.bo.User;
 
+public interface IAdministrationCommonAsync {
+	public void registerUser(String email, String password);
+	public void loginUser(String email, String password);
+	public void logoutUser();
+	public void getElementMapper();
+	public void getModuleMapper();
+	public void getProductMapper();
+	public void getPartlistMapper();
+	public void getUserMapper();
+	public void setUser(User user);
+	public void getUser();
 }
