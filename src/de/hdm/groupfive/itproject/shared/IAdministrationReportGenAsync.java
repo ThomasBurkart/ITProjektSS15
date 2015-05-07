7ca21700a5863ad1aa59ae34cebf001b3c1b6903
TimoFesseler <timo.fesseler@gmail.com>
2015-05-07 09:45:37 +0200

    Interface IAdministrationReportGenAsync hinzugefügt

--- src/de/hdm/groupfive/itproject/shared/IAdministrationReportGenAsync.java
+++ src/de/hdm/groupfive/itproject/shared/IAdministrationReportGenAsync.java
@@ -1,5 +1,14 @@
 package de.hdm.groupfive.itproject.shared;
 
-public interface IAdministrationReportGenAsync {
+import de.hdm.groupfive.itproject.shared.bo.Module;
+import de.hdm.groupfive.itproject.shared.bo.Partlist;
 
+
+public interface IAdministrationReportGenAsync {
+	public void findPartlistByModuleName(String name);
+	public void findPartlistByModuleId(int id);
+	public void findPartlistById(int id);
+	public void findPartlistByModule(Module module);
+	public void getAllProducts();
+	public void calculateMaterial(Partlist partlist);
 }
