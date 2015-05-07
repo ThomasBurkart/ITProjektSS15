5f5da9aba49b913632dec5ebe5216820c6bda9b4
TimoFesseler <timo.fesseler@gmail.com>
2015-05-07 10:25:31 +0200

    Methodenköpfe und Variablen nach KLassendiagramm in Partlist angelegt

--- src/de/hdm/groupfive/itproject/shared/bo/Partlist.java
+++ src/de/hdm/groupfive/itproject/shared/bo/Partlist.java
@@ -1,6 +1,7 @@
 package de.hdm.groupfive.itproject.shared.bo;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Partlist extends HashMap<Element,Integer> implements Serializable {
@@ -12,6 +13,53 @@ public class Partlist extends HashMap<Element,Integer> implements Serializable {
 	 * @param element
 	 * @return
 	 */
+	
+	private Date creationDate;
+	private String name;
+	private int id;
+	
+	public void add(Element element, int quantity){
+		Element.
+	}
+	
+	public void edit (Element element){
+		
+	}
+	
+	public void delete(Element element){
+		
+	}
+	
+	public Element deleteById(Element element){
+		
+	}
+	
+	public Element getElementById(Element element){
+		
+	}
+	
+	public static int getId(){
+		
+	}
+	
+	public Date getCreationDate(){
+		
+	}
+	
+	public int getQuantityByElement(Element element){
+		
+	}
+	
+	public String getName(){
+		
+	}
+	
+	public void setName(String name){
+		
+	}
+	
+	
+	
 	public int getQuantityByElement(Element element) {
 		int result = -1;
 		if (this.containsKey(element)) {
