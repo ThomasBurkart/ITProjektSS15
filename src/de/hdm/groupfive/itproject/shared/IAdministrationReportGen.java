1e2fc9393b3d9f069bf87479832c1225c67ab33d
TimoFesseler <timo.fesseler@gmail.com>
2015-05-07 10:09:58 +0200

    AsyncCallback hatte ich davor vergessen, ist jetzt hinzugefügt, inkl. abgeänderte Beschreibung von Prof. Thies

--- src/de/hdm/groupfive/itproject/shared/IAdministrationReportGen.java
+++ src/de/hdm/groupfive/itproject/shared/IAdministrationReportGen.java
@@ -5,9 +5,14 @@ import de.hdm.groupfive.itproject.shared.bo.*;
 
 public interface IAdministrationReportGen {
 	public Partlist findPartlistByModuleName(String name);
+
 	public Partlist findPartlistByModuleId(int id);
+
 	public Partlist findPartlistById(int id);
+
 	public Partlist findPartlistByModule(Module module);
+
 	public Vector<Product> getAllProducts();
+
 	public Object calculateMaterial(Partlist partlist);
 }
--- src/de/hdm/groupfive/itproject/shared/IAdministrationReportGenAsync.java
+++ src/de/hdm/groupfive/itproject/shared/IAdministrationReportGenAsync.java
@@ -1,14 +1,36 @@
 package de.hdm.groupfive.itproject.shared;
 
+import java.util.Vector;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import de.hdm.groupfive.itproject.shared.bo.Module;
 import de.hdm.groupfive.itproject.shared.bo.Partlist;
+import de.hdm.groupfive.itproject.shared.bo.Product;
+import de.hdm.groupfive.itproject.shared.IAdministrationReportGen;
 
+/**
+ * Das asynchrone Gegenstück des Interface {@link IAdministrationReportGen}. Es
+ * wird semiautomatisch durch das Google Plugin erstellt und gepflegt. Daher
+ * erfolgt hier keine weitere Dokumentation. Für weitere Informationen siehe das
+ * synchrone Interface {@link IAdministrationReportGen}.
+ * 
+ * @author thies
+ * @author fesseler (update)
+ */
 
 public interface IAdministrationReportGenAsync {
-	public void findPartlistByModuleName(String name);
-	public void findPartlistByModuleId(int id);
-	public void findPartlistById(int id);
-	public void findPartlistByModule(Module module);
-	public void getAllProducts();
-	public void calculateMaterial(Partlist partlist);
+	public void findPartlistByModuleName(String name,
+			AsyncCallback<Partlist> callback);
+
+	public void findPartlistByModuleId(int id, AsyncCallback<Partlist> callback);
+
+	public void findPartlistById(int id, AsyncCallback<Partlist> callback);
+
+	public void findPartlistByModule(Module module,
+			AsyncCallback<Partlist> callback);
+
+	public void getAllProducts(AsyncCallback<Vector<Product>> callback);
+
+	public void calculateMaterial(Partlist partlist,
+			AsyncCallback<Object> callback);
 }
