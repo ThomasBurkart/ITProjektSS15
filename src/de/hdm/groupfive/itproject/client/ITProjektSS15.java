26fa920b3f1d7a2c4f971219b9e3ca25fb472527
Thomas Burkart <tb085@hdm-stuttgart.de>
2015-05-28 14:24:19 +0200

    Login Dialog in Extra Klasse ausgelagert, damit sie auch von ReportGen verwendet werden kann

--- src/de/hdm/groupfive/itproject/client/ITProjektSS15.java
+++ src/de/hdm/groupfive/itproject/client/ITProjektSS15.java
@@ -18,104 +18,18 @@ public class ITProjektSS15 implements EntryPoint {
 		HTML content = new HTML("Herzlich Willkommen :)");
     	RootPanel.get("main").clear();
     	RootPanel.get("main").add(content);
-    	// ladet den Login Dialog 
-		loadLoginDialog();
+    	
+    	// lädt den Login Dialog 
+    	LoginLogout login = new LoginLogout();
+		login.loadDialog();
 		
 		// erzeugt das Menü links auf der Seite
     	createMenu();
 	}
 	
-	private void loadLoginDialog() {
-		 final DialogBox dialogBox = createLoginDialogBox();
-		 dialogBox.setGlassEnabled(true);
-		 dialogBox.setAnimationEnabled(true);
-		 dialogBox.center();
-         dialogBox.show();
-	}
 	
-	private DialogBox createLoginDialogBox() {
-	    // Create a dialog box and set the caption text
-	    final DialogBox dialogBox = new DialogBox();
-	    dialogBox.setText("Anmelden");
-	    dialogBox.setModal(true);
-
-	    final Grid grid = new Grid(3, 2);
-	    grid.setCellSpacing(50);
-	    grid.setCellPadding(50);
-	    grid.setWidget(0, 0, new HTML("E-Mail"));
-	    final TextBox emailBox =  new TextBox();
-	    emailBox.setWidth("220px");
-	    grid.setWidget(0, 1, emailBox);
-
-	    grid.setWidget(1, 0, new HTML("Passwort"));
-	    final PasswordTextBox passwordBox = new PasswordTextBox();
-	    passwordBox.setWidth("220px");
-	    grid.setWidget(1, 1, passwordBox);
-	    
-	    // Create a table to layout the content
-	    //VerticalPanel dialogContents = new VerticalPanel();
-//	    dialogContents.setSpacing(4);
-//	    dialogBox.setWidget(dialogContents);
-//
-//	    // Add some text to the top of the dialog
-//	    HTML details = new HTML("TopText");
-//	    dialogContents.add(details);
-//	    dialogContents.setCellHorizontalAlignment(
-//	        details, HasHorizontalAlignment.ALIGN_CENTER);
-
-	    // Add a close button at the bottom of the dialog
-	    Button loginButton = new Button("anmelden", new ClickHandler() {
-	          public void onClick(ClickEvent event) {
-	            String email = emailBox.getValue();
-	            String pass = passwordBox.getValue();
-	            // TODO: Methode zum einloggen eines Benutzers aufrufen -> onSuccess/OnFailure Handling?!
-	            dialogBox.hide();
-	            
-	          }
-	        });
-	    loginButton.setWidth("100px");
-	    Button regButton = new Button("registrieren", new ClickHandler() {
-	          public void onClick(ClickEvent event) {
-	        	dialogBox.setText("Registration");
-	        	grid.resizeRows(6);
-	        	grid.setWidget(2, 0, new HTML("Passwort wiederholen"));
-	    	    final PasswordTextBox passwordBox2 = new PasswordTextBox();
-	    	    passwordBox2.setWidth("220px");
-	    	    grid.setWidget(2, 1, passwordBox2);
-	    	    
-	    	    grid.setWidget(3, 0, new HTML("Vorname"));
-	    	    final TextBox firstNameBox =  new TextBox();
-	    	    firstNameBox.setWidth("220px");
-	    	    grid.setWidget(3, 1, firstNameBox);
-	    	    
-	    	    grid.setWidget(4, 0, new HTML("Name"));
-	    	    final TextBox nameBox =  new TextBox();
-	    	    nameBox.setWidth("220px");
-	    	    grid.setWidget(4, 1, nameBox);
-	            //dialogBox.hide();
-	    	    Button regButton = new Button("registrieren", new ClickHandler() {
-	  	          public void onClick(ClickEvent event) {
-	  	        	  // TODO: Methode zum registrieren eines neuen Benutzers aufrufen.
-	  	        	  dialogBox.hide();
-	  	          }
-	    	    });
-	    	    regButton.setWidth("220px");
-	    	    grid.setWidget(5, 1, regButton);
-	          }
-	        });
-	    regButton.setWidth("100px");
-	    HorizontalPanel actionPanel = new HorizontalPanel();
-	    actionPanel.add(regButton);
-	    actionPanel.add(loginButton);
-	    grid.setWidget(2, 1, actionPanel);
-	    VerticalPanel dialogContent = new VerticalPanel();
-	    HTML details = new HTML("Bitte geben Sie Ihre Benutzerdaten ein, um sich<br />am System anzumelden.");
-	    dialogContent.add(details);
-	    dialogContent.add(grid);
-	    dialogBox.setWidget(dialogContent);
-	    // Return the dialog box
-	    return dialogBox;
-	  }
+	
+	
 
 	
 	private void createMenu() {
