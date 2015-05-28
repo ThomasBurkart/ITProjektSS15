9a5392c06c96c41e82192227dc1a5548761d0ca8
Thomas Burkart <tb085@hdm-stuttgart.de>
2015-05-28 18:04:54 +0200

    Report Generator überarbeitet, damit dieser als seperater Einstiegspunkt verwendet werden kann. Layout von beiden Clients angepasst.

--- src/de/hdm/groupfive/itproject/client/ITProjektSS15.java
+++ src/de/hdm/groupfive/itproject/client/ITProjektSS15.java
@@ -14,23 +14,37 @@ public class ITProjektSS15 implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+    	// lädt den Login Dialog 
+    	LoginLogout login = new LoginLogout();
+		login.loadDialog();
 		
 		HTML content = new HTML("Herzlich Willkommen :)");
     	RootPanel.get("main").clear();
     	RootPanel.get("main").add(content);
     	
-    	// lädt den Login Dialog 
-    	LoginLogout login = new LoginLogout();
-		login.loadDialog();
+		VerticalPanel searchPanel = new VerticalPanel();
+		searchPanel.setStylePrimaryName("searchPanel");
+		FlowPanel searchInputPanel = new FlowPanel();
+		TextBox searchBox = new TextBox();
+		searchBox.setStylePrimaryName("searchBox col-md-9 col-sm-9 col-xs-9");	
+		//searchBox.setWidth("100%");
+		searchBox.setFocus(true);
+		searchBox.setTitle("Suche nach Bauteil, Baugruppe oder Enderzeugnis ...");
+		searchBox.getElement().setPropertyString("placeholder", "Suche nach Bauteil, Baugruppe oder Enderzeugnis ...");
+		
+		Button searchBtn = new Button();
+		
+		searchBtn.setStylePrimaryName("btn btn-default col-md-2 col-sm-2 col-xs-2");
+		searchBtn.setText("Suchen");
+		searchInputPanel.add(searchBox);
+		searchInputPanel.add(searchBtn);
+		searchPanel.add(searchInputPanel);
+		RootPanel.get("navigator").add(searchPanel);
 		
 		// erzeugt das Menü links auf der Seite
-    	createMenu();
+    	//createMenu();
 	}
 	
-	
-	
-	
-
 	
 	private void createMenu() {
 		 /*
@@ -54,131 +68,5 @@ public class ITProjektSS15 implements EntryPoint {
 	    
 	  
 	    
-	    DecoratedStackPanel stackPanel = new DecoratedStackPanel();
-	    stackPanel.setWidth("100%");
-	    stackPanel.setStylePrimaryName("menupanel");
-	    
-	    String elementHeader = getHeaderString("+ Bauteil");
-	    stackPanel.add(createElementLinks(), elementHeader, true);
-	    String elementHeader2 = getHeaderString("+ Baugruppe");
-	    stackPanel.add(createModuleLinks(), elementHeader2, true);
-	    String elementHeader3 = getHeaderString("+ Enderzeugnis");
-	    stackPanel.add(createElementLinks(), elementHeader3, true);
-
-	    RootPanel.get("navigator").add(stackPanel);
 	}
-	
-	private VerticalPanel createElementLinks() {
-		VerticalPanel panel = new VerticalPanel();
-		panel.setWidth("100%");
-		final Hyperlink addLink = new Hyperlink("erstellen", "addElement");
-		addLink.setStylePrimaryName("menubutton"); 
-		ClickHandler addClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Bauteil erstellen geklickt :D");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		addLink.addDomHandler(addClickHandler, ClickEvent.getType());
-		panel.add(addLink);
-		
-		final Hyperlink editLink = new Hyperlink("ändern", "editElement");
-		editLink.setStylePrimaryName("menubutton"); 
-		ClickHandler editClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Bauteil ändern geklickt :P");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		editLink.addDomHandler(editClickHandler, ClickEvent.getType());
-		panel.add(editLink);
-		
-		final Hyperlink deleteLink = new Hyperlink("löschen", "deleteElement");
-		deleteLink.setStylePrimaryName("menubutton"); 
-		ClickHandler deleteClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Bauteil löschen geklickt :D");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		deleteLink.addDomHandler(deleteClickHandler, ClickEvent.getType());
-		panel.add(deleteLink);
-		return panel;
-	}
-	
-	private VerticalPanel createModuleLinks() {
-		VerticalPanel panel = new VerticalPanel();
-		panel.setWidth("100%");
-		final Hyperlink addLink = new Hyperlink("erstellen", "addModule");
-		addLink.setStylePrimaryName("menubutton"); 
-		ClickHandler addClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Baugruppe erstellen geklickt :D");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		addLink.addDomHandler(addClickHandler, ClickEvent.getType());
-		panel.add(addLink);
-		
-		final Hyperlink editLink = new Hyperlink("ändern", "editModule");
-		editLink.setStylePrimaryName("menubutton"); 
-		ClickHandler editClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Baugruppe �ndern geklickt :D");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		editLink.addDomHandler(editClickHandler, ClickEvent.getType());
-		panel.add(editLink);
-		
-		final Hyperlink deleteLink = new Hyperlink("löschen", "deleteModule");
-		deleteLink.setStylePrimaryName("menubutton"); 
-		ClickHandler deleteClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Baugruppe löschen geklickt :D");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		deleteLink.addDomHandler(deleteClickHandler, ClickEvent.getType());
-		panel.add(deleteLink);
-		
-		final Hyperlink defineLink = new Hyperlink("zuordnen", "defineModule");
-		defineLink.setStylePrimaryName("menubutton"); 
-		ClickHandler defineClickHandler = new ClickHandler() {
-		    public void onClick(ClickEvent event) {
-		    	final HTML content = new HTML("Baugruppe zuordnen geklickt :D");
-		    	RootPanel.get("main").clear();
-		    	RootPanel.get("main").add(content);
-		    }
-		};
-		defineLink.addDomHandler(defineClickHandler, ClickEvent.getType());
-		panel.add(defineLink);
-		
-		return panel;
-	}
-	
-	/**
-	   * Create the {@link Tree} of Mail options.
-	   *
-	   * @param images the {@link Images} used in the Mail options
-	   * @return the {@link Tree} of mail options
-	   */
-	  
-	private String getHeaderString(String text) {
-	    // Add the image and text to a horizontal panel
-	    HorizontalPanel hPanel = new HorizontalPanel();
-	    hPanel.setSpacing(0);
-	    hPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
-	    HTML headerText = new HTML(text);
-	    hPanel.add(headerText);
-
-	    // Return the HTML string for the panel
-	    return hPanel.getElement().getString();
-	  }
 }
--- src/de/hdm/groupfive/itproject/client/ReportGen.java
+++ src/de/hdm/groupfive/itproject/client/ReportGen.java
@@ -1,21 +1,15 @@
 package de.hdm.groupfive.itproject.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Hyperlink;
-import com.google.gwt.user.client.ui.RootPanel;
 
 public class ReportGen implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
-		
-	
 
-	 //l‰dt den Login Dialog 
-    //	LoginLogout login = new LoginLogout();
-		//login.loadDialog();
-	    
+		// l√§dt den Login Dialog 
+    	LoginLogout login = new LoginLogout();
+		login.loadDialog();
 	}
 
 }
