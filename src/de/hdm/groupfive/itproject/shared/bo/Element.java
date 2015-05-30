67df20199be070660b0dcab557262916589f65f9
Thomas Burkart <tb085@hdm-stuttgart.de>
2015-05-30 16:28:28 +0200

    Methode MaterialDescription war doppelt vorhanden.

--- src/de/hdm/groupfive/itproject/shared/bo/Element.java
+++ src/de/hdm/groupfive/itproject/shared/bo/Element.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 public class Element extends BusinessObject{
 	
 	/**
-	 * Eindeutige SerialVersion Id. Wird zum Serialisieren der Klasse benötigt.
+	 * Eindeutige SerialVersion Id. Wird zum Serialisieren der Klasse benï¿½tigt.
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -54,13 +54,6 @@ public class Element extends BusinessObject{
 	 */
 	public String getDescription() {
 		return this.description;
-	}
-
-	/**
-	 * Auslesen der Materialbeschreibung des Bauteils
-	 */
-	public String getMaterialdescription() {
-		return this.materialDescription;
 	}
 
 	/**
@@ -131,7 +124,7 @@ public class Element extends BusinessObject{
 
 
 	/**
-	 * Erzeugen einer einfachen textuellen Repräsentation der jeweiligen
+	 * Erzeugen einer einfachen textuellen Reprï¿½sentation der jeweiligen
 	 * Kontoinstanz.
 	 */
 	@Override
@@ -143,19 +136,19 @@ public class Element extends BusinessObject{
 	 * <p>
 	 * Feststellen der <em>inhaltlichen</em> Gleichheit zweier Account-Objekte.
 	 * Die Gleichheit wird in diesem Beispiel auf eine identische Kontonummer
-	 * beschränkt.
+	 * beschrï¿½nkt.
 	 * </p>
 	 * <p>
 	 * <b>ACHTUNG:</b> Die inhaltliche Gleichheit nicht mit dem Vergleich der
-	 * <em>Identität</em> eines Objekts mit einem anderen verwechseln!!! Dies
-	 * würde durch den Operator <code>==</code> bestimmt. Bei Unklarheit hierzu
-	 * können Sie nocheinmal in die Definition des Sprachkerns von Java schauen.
-	 * Die Methode <code>equals(...)</code> ist für jeden Referenzdatentyp
+	 * <em>Identitï¿½t</em> eines Objekts mit einem anderen verwechseln!!! Dies
+	 * wï¿½rde durch den Operator <code>==</code> bestimmt. Bei Unklarheit hierzu
+	 * kï¿½nnen Sie nocheinmal in die Definition des Sprachkerns von Java schauen.
+	 * Die Methode <code>equals(...)</code> ist fï¿½r jeden Referenzdatentyp
 	 * definiert, da sie bereits in der Klasse <code>Object</code> in
 	 * einfachster Form realisiert ist. Dort ist sie allerdings auf die simple
 	 * Bestimmung der Gleicheit der Java-internen Objekt-ID der verglichenen
-	 * Objekte beschränkt. In unseren eigenen Klassen können wir diese Methode
-	 * überschreiben und ihr mehr Intelligenz verleihen.
+	 * Objekte beschrï¿½nkt. In unseren eigenen Klassen kï¿½nnen wir diese Methode
+	 * ï¿½berschreiben und ihr mehr Intelligenz verleihen.
 	 * </p>
 	 */
 	@Override
