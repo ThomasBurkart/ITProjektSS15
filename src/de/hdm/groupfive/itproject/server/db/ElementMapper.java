1efd1b785ad8788e7fa4e30d0def48eca17c78f2
Samson <Samson@samires-air>
2015-06-12 01:35:40 +0200

    Element Mapper so gut wie fertig, letzte Teil muss noch betrachtet werden. Fehlerbehebungen sind vorbehalten

--- src/de/hdm/groupfive/itproject/server/db/ElementMapper.java
+++ src/de/hdm/groupfive/itproject/server/db/ElementMapper.java
@@ -6,6 +6,7 @@ import java.util.Vector;
 import de.hdm.groupfive.itproject.shared.bo.Element;
 import de.hdm.groupfive.itproject.shared.bo.Product;
 import de.hdm.groupfive.itproject.shared.bo.User;
+//** @ author Jakupi, Samire ; Thies
 
 
 public class ElementMapper {
@@ -64,7 +65,9 @@ public class ElementMapper {
 	
 	/**
 	 * Auslesen aller Elements
-	 * @return
+	 * @return  Ein Vektor mit Element-Objekten, die sämtliche Elemente
+     *          repräsentieren. Bei evtl. Exceptions wird ein partiell gefüllter
+     *          oder ggf. auch leerer Vetor zurückgeliefert.
 	 */
 	public Vector<Element> findAll() {
 		Connection con = DBConnection.connection();
@@ -99,41 +102,128 @@ public class ElementMapper {
 	
 	/**
 	 * 
-	 * @param p
-	 * @return
+	 * Einfügen eines <code>Element</code>-Objekts in die Datenbank. Dabei wird
+	 * auch der Primärschlüssel des übergebenen Objekts geprüft und ggf.
+	 * berichtigt.
+	 * 
+	 * @param e das zu speichernde Objekt
+	 * @return das bereits übergebene Objekt, jedoch mit ggf. korrigierter
+	 *         <code>id</code>.
 	 */
 	public Element insert(Element e){
+		Connection con = DBConnection.connection ();
 		
-		return null;
+		try {
+			Statement stmt = con.createStatement ();
+			
+			/*
+			 * Zunächst schauen wir nach, welches der momentan höhste 
+			 * Primärschlüsselwert ist.
+			 */
+			ResultSet rs = stmt.executeQuery ("SELECT MAX(id) AS maxid" + "FROM element");
+			// Wenn wir etwas zurückhalten, kann dies nur einzeilig sein
+			if (rs.next()) {
+				/*
+				 * e erhaelt den bisher maximalen, nun um 1 inkrementierten
+				 * Primaerschluessel
+				 */
+				e.setId (rs.getInt ("maxid") + 1);
+				stmt = con.createStatement ();
+				
+				// die tatsaechliche Einfuegeoperation
+				stmt.executeUpdate("INSERT INTO element (id, name, description, material_description, creation_date, last_update)"
+								  + "VALUES ("
+						          + e.getId()
+						          + ",'"
+						          + e.getName()
+						          + "','"
+						          + e.getDescription()
+						          + "','"
+						          + e.getMaterialDescription()
+						          + "',"
+						          + e.getCreationDate()
+						          + ","
+						          + e.getLastUpdate() + ")");
+			}
+		} catch (SQLException e2) {
+			e2.printStackTrace ();
+		}
+		/*
+		 * Rückgabe, des evtl. korrigierten Elements.
+		 * 
+		 * HINWEIS: Da in Java nur Referenzen auf Objekte und keine physischen
+		 * Objekte übergeben werden, ware die Anpassung des Dozent-Objekts auch
+		 * ohne diese explizite Rückgabe außerhalb dieser Methode sichtbar.
+		 * Die explizite Rückgabe von d ist eher ein Stilmittel, um zu
+		 * signalisieren, dass sich das Objekt evtl. im Laufe der Methode
+		 * verändert hat.
+		 */
+			return e;
 	}
+	
+		/** Wiederholtes Schreiben eines Objektes in die Datenbank
+		 * 
+		 * @param e  das Objekt, das in die Datenbank geschrieben werden soll
+		 * @return das als Parameter übergebene Objekt
+		 */
+	
+		public Element update(Element e) {
+			Connection con = DBConnection.connection ();
+			
+			try {
+				Statement stmt = con.createStatement ();
+				
+				stmt.executeUpdate ("UPDATE elements SET" + "name= '"
+								   + e.getName() + "'," + "description= '"
+								   + e.getDescription() + "'," + "material_description= '"
+								   + e.getMaterialDescription() + "'," + "creation_date= '"
+								   + e.getCreationDate() + "'," + "last_update= '"
+								   + e.getLastUpdate() + "'," + "WHERE id="
+								   + e.getId());
+								   
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+			
+			// Um die Analogie zu insert(Element e) zu wahren, geben wir e zurück
+			return e;
+		}
+		
+		/** Löschen der Daten eines <code>Element</code> - Objekts aus der Datenbank
+		 * 
+		 * @param e das aus der DB zu löschende "Objekt"
+		 */
+		
+		public void delete(Element e) {
+			Connection con = DBConnection.connection();
+			
+			try {
+				Statement stmt = con.createStatement();
+				
+				stmt.executeUpdate("DELETE FROM element" + "WHERE id=" + e.getId());
+				}
 
-	/**
-	 * 
-	 * @param product
-	 */
-	public void delete(Element e) {
-		// TODO Auto-generated method stub
+			catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
 		
-	}
+		/**  
+		 * Löschen sämtlicher (Bauteile) Elements (<code>Element</code>-Objekte) einer Baugruppe.
+         * Diese Methode sollte aufgerufen werden, bevor ein <code>Baugruppe</code>
+         * -Objekt gelöscht wird.
+         * 
+         * @param b das <code>Baugruppe</code>-Objekt, zu dem die Bauteile gehören
+         */
+		
+		
+		
+		
+		
+
 	
 	
-	/**
-	 * 
-	 */
-	public Element update(Element e){
-		return null;
-	}
 	
-	/**
-	 * 
-	 */
-	public static ElementMapper elementMapper(){
-		return null;
-	}
-	
-	/**
-	 * 
-	 */
 	public Element findByName(String name){
 		return null;
 	}
