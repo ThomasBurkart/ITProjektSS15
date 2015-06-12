376af42264ea67e995212a81a1d0d355526da025
Thomas Burkart <tb085@hdm-stuttgart.de>
2015-06-12 11:27:18 +0200

    ElementMapper nach dem GIT Hub Conflict nochmal angepasst.

--- src/de/hdm/groupfive/itproject/server/db/ElementMapper.java
+++ src/de/hdm/groupfive/itproject/server/db/ElementMapper.java
@@ -8,6 +8,7 @@ import com.google.protos.cloud.sql.Client.SqlException;
 import de.hdm.groupfive.itproject.shared.bo.Element;
 import de.hdm.groupfive.itproject.shared.bo.Product;
 import de.hdm.groupfive.itproject.shared.bo.User;
+
 //** @ author Jakupi, Samire ; Thies
 
 public class ElementMapper {
@@ -25,40 +26,15 @@ public class ElementMapper {
 
 		return elementMapper;
 	}
-<<<<<<< HEAD
-	
-	public Element findByKey(int id) {
-		//DB Verbindung hier holen
-=======
 
 	public Element findById(int id) throws SQLException {
 		Element result = null;
 		// DB Verbindung hier holen
->>>>>>> refs/heads/Thomas
 		Connection con = DBConnection.connection();
 
 		try {
 			Statement stmt = con.createStatement();
-<<<<<<< HEAD
-			//Statement ausfuellen und als Query an die DB schicken
-			
-			ResultSet rs = stmt.executeQuery("SELECT id,name, description, material_description, creation_date, last_update FROM element" 
-					+ "WHERE id =" + id);
-			//ORDER BY nochmal anschauen und schauen ob es eingebaut werden muss
-			
-			if ( rs.next()){
-				
-				Element e = new Element ();
-				e.setId (rs.getInt ("id"));
-				e.setName (rs.getString ("name"));
-				e.setDescription (rs.getString ("description"));
-				e.setMaterialDescription(rs.getString ("material_description"));
-				e.setCreationDate (rs.getDate ("creation_date"));
-				e.setLastUpdate (rs.getDate ("last_update"));
-				
-				return e;
-			
-=======
+
 			// Statement ausfuellen und als Query an die DB schicken
 
 			ResultSet rs = stmt.executeQuery("SELECT * FROM element"
@@ -75,7 +51,6 @@ public class ElementMapper {
 
 				result = e;
 
->>>>>>> refs/heads/Thomas
 			}
 		} catch (SQLException ex) {
 			throw ex;
@@ -93,14 +68,10 @@ public class ElementMapper {
 
 	/**
 	 * Auslesen aller Elements
-<<<<<<< HEAD
-	 * @return  Ein Vektor mit Element-Objekten, die sämtliche Elemente
-     *          repräsentieren. Bei evtl. Exceptions wird ein partiell gefüllter
-     *          oder ggf. auch leerer Vetor zurückgeliefert.
-=======
 	 * 
-	 * @return
->>>>>>> refs/heads/Thomas
+	 * @return Ein Vektor mit Element-Objekten, die sämtliche Elemente
+	 *         repräsentieren. Bei evtl. Exceptions wird ein partiell gefüllter
+	 *         oder ggf. auch leerer Vetor zurückgeliefert.
 	 */
 	public Vector<Element> findAll() throws SQLException {
 		Connection con = DBConnection.connection();
@@ -109,52 +80,26 @@ public class ElementMapper {
 
 		try {
 			Statement stmt = con.createStatement();
-<<<<<<< HEAD
-			
-			ResultSet rs = stmt.executeQuery ("SELECT id, name, description, material_description, creation_date, last_update FROM element" 
-=======
 
 			ResultSet rs = stmt.executeQuery("SELECT * FROM element"
->>>>>>> refs/heads/Thomas
 					+ "ORDER BY id");
 			// Für jeden Eintrag im Suchergebnis wird nun ein Element Objekt
 			// erstellt
 
 			while (rs.next()) {
 				Element e = new Element();
-<<<<<<< HEAD
-				e.setId (rs.getInt ("id"));
-				e.setName (rs.getString ("name"));
-				e.setDescription (rs.getString ("description"));
-				e.setMaterialDescription (rs.getString ("material_description"));
-				e.setCreationDate (rs.getDate ("creation_date"));
-				e.setLastUpdate (rs.getDate ("last_update"));
-				
-=======
 				e.setId(rs.getInt("id"));
 				e.setName(rs.getString("name"));
 				e.setDescription(rs.getString("description"));
-				e.setMaterialDescription(rs.getString("material description"));
-				e.setCreationDate(rs.getDate("int columnIndex, Calendar cal"));
-				e.setLastUpdate(rs.getDate("int columnIndex, Calendar cal"));
+				e.setMaterialDescription(rs.getString("material_description"));
+				e.setCreationDate(rs.getDate("creation_date"));
+				e.setLastUpdate(rs.getDate("last_update"));
 
->>>>>>> refs/heads/Thomas
 				result.addElement(e);
 			}
-<<<<<<< HEAD
-		} 
-		    catch (SQLException e2) {
-			e2.printStackTrace();
-=======
 		} catch (SQLException ex) {
 			throw ex;
->>>>>>> refs/heads/Thomas
 		}
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> refs/heads/Thomas
 		return result;
 	}
 
@@ -164,96 +109,98 @@ public class ElementMapper {
 	 * auch der Primärschlüssel des übergebenen Objekts geprüft und ggf.
 	 * berichtigt.
 	 * 
-	 * @param e das zu speichernde Objekt
+	 * @param e
+	 *            das zu speichernde Objekt
 	 * @return das bereits übergebene Objekt, jedoch mit ggf. korrigierter
 	 *         <code>id</code>.
 	 */
-<<<<<<< HEAD
-	public Element insert(Element e){
-		Connection con = DBConnection.connection ();
-		
+	public Element insert(Element e) throws SQLException {
+		Connection con = DBConnection.connection();
+
 		try {
-			Statement stmt = con.createStatement ();
-			
+			Statement stmt = con.createStatement();
+
 			/*
-			 * Zunächst schauen wir nach, welches der momentan höhste 
+			 * Zunächst schauen wir nach, welches der momentan höhste
 			 * Primärschlüsselwert ist.
 			 */
-			ResultSet rs = stmt.executeQuery ("SELECT MAX(id) AS maxid" + "FROM element");
+			ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid"
+					+ "FROM element");
 			// Wenn wir etwas zurückhalten, kann dies nur einzeilig sein
 			if (rs.next()) {
 				/*
 				 * e erhaelt den bisher maximalen, nun um 1 inkrementierten
 				 * Primaerschluessel
 				 */
-				e.setId (rs.getInt ("maxid") + 1);
-				stmt = con.createStatement ();
-				
+				e.setId(rs.getInt("maxid") + 1);
+				stmt = con.createStatement();
+
 				// die tatsaechliche Einfuegeoperation
 				stmt.executeUpdate("INSERT INTO element (id, name, description, material_description, creation_date, last_update)"
-								  + "VALUES ("
-						          + e.getId()
-						          + ",'"
-						          + e.getName()
-						          + "','"
-						          + e.getDescription()
-						          + "','"
-						          + e.getMaterialDescription()
-						          + "',"
-						          + e.getCreationDate()
-						          + ","
-						          + e.getLastUpdate() + ")");
+						+ "VALUES ("
+						+ e.getId()
+						+ ",'"
+						+ e.getName()
+						+ "','"
+						+ e.getDescription()
+						+ "','"
+						+ e.getMaterialDescription()
+						+ "',"
+						+ e.getCreationDate() + "," + e.getLastUpdate() + ")");
 			}
 		} catch (SQLException e2) {
-			e2.printStackTrace ();
+			throw e2;
 		}
 		/*
 		 * Rückgabe, des evtl. korrigierten Elements.
 		 * 
 		 * HINWEIS: Da in Java nur Referenzen auf Objekte und keine physischen
 		 * Objekte übergeben werden, ware die Anpassung des Dozent-Objekts auch
-		 * ohne diese explizite Rückgabe außerhalb dieser Methode sichtbar.
-		 * Die explizite Rückgabe von d ist eher ein Stilmittel, um zu
+		 * ohne diese explizite Rückgabe außerhalb dieser Methode sichtbar. Die
+		 * explizite Rückgabe von d ist eher ein Stilmittel, um zu
 		 * signalisieren, dass sich das Objekt evtl. im Laufe der Methode
 		 * verändert hat.
 		 */
-			return e;
+		return e;
 	}
-	
-		/** Wiederholtes Schreiben eines Objektes in die Datenbank
-		 * 
-		 * @param e  das Objekt, das in die Datenbank geschrieben werden soll
-		 * @return das als Parameter übergebene Objekt
-		 */
-	
-		public Element update(Element e) {
-			Connection con = DBConnection.connection ();
-			
-			try {
-				Statement stmt = con.createStatement ();
-				
-				stmt.executeUpdate ("UPDATE elements SET" + "name= '"
-								   + e.getName() + "'," + "description= '"
-								   + e.getDescription() + "'," + "material_description= '"
-								   + e.getMaterialDescription() + "'," + "creation_date= '"
-								   + e.getCreationDate() + "'," + "last_update= '"
-								   + e.getLastUpdate() + "'," + "WHERE id="
-								   + e.getId());
-								   
-			} catch (SQLException e2) {
-				e2.printStackTrace();
-			}
-			
-			// Um die Analogie zu insert(Element e) zu wahren, geben wir e zurück
-			return e;
+
+	/**
+	 * Wiederholtes Schreiben eines Objektes in die Datenbank
+	 * 
+	 * @param e
+	 *            das Objekt, das in die Datenbank geschrieben werden soll
+	 * @return das als Parameter übergebene Objekt
+	 */
+
+	public Element update(Element e) throws SQLException {
+		Connection con = DBConnection.connection();
+
+		try {
+			Statement stmt = con.createStatement();
+
+			stmt.executeUpdate("UPDATE elements SET" + "name= '" + e.getName()
+					+ "'," + "description= '" + e.getDescription() + "',"
+					+ "material_description= '" + e.getMaterialDescription()
+					+ "'," + "creation_date= '" + e.getCreationDate() + "',"
+					+ "last_update= '" + e.getLastUpdate() + "'," + "WHERE id="
+					+ e.getId());
+
+		} catch (SQLException e2) {
+			throw e2;
 		}
-		
-		/** Löschen der Daten eines <code>Element</code> - Objekts aus der Datenbank
-		 * 
-		 * @param e das aus der DB zu löschende "Objekt"
-		 */
-		
-		public void delete(Element e) {
+
+		// Um die Analogie zu insert(Element e) zu wahren, geben wir e zurück
+		return e;
+	}
+
+	/**
+	 * Löschen der Daten eines <code>Element</code> - Objekts aus der Datenbank
+	 * 
+	 * @param e
+	 *            das aus der DB zu löschende "Objekt"
+	 */
+
+	public void delete(Element e) throws SQLException {
 			Connection con = DBConnection.connection();
 			
 			try {
@@ -263,30 +210,10 @@ public class ElementMapper {
 				}
 
 			catch (SQLException e2) {
-				e2.printStackTrace();
+				throw e2;
 			}
 		}
-		
-		/**  
-		 * Löschen sämtlicher (Bauteile) Elements (<code>Element</code>-Objekte) einer Baugruppe.
-         * Diese Methode sollte aufgerufen werden, bevor ein <code>Baugruppe</code>
-         * -Objekt gelöscht wird.
-         * 
-         * @param b das <code>Baugruppe</code>-Objekt, zu dem die Bauteile gehören
-         */
-		
-		
-		
-		
-		
 
-	
-	
-	
-	public Element findByName(String name){
-		return null;
-	}
-	
 	/**
 	 * 
 	 * @return
@@ -294,44 +221,6 @@ public class ElementMapper {
 	public Vector<Element> getAllElements() {
 		// TODO Auto-generated method stub
 		return null;
-=======
-	public Element insert(Element e) throws SQLException {
-		return null;
-	}
-
-	/**
-	 * Löscht ein Element aus der Datenbank
-	 * @param Element e
-	 */
-	public void delete(Element e) throws SQLException {
-		Connection con = DBConnection.connection();
-		try {
-			Statement stmt = con.createStatement();
-			stmt.executeUpdate("DELETE FROM element " + "WHERE id=" + e.getId());
-		} catch (SQLException ex) {
-			throw ex;
-		}
->>>>>>> refs/heads/Thomas
-	}
-
-	/**
-	 * 
-	 */
-	public Element update(Element e) throws SQLException {
-		Connection con = DBConnection.connection();
-		// TODO: evtl. weitere Spalten namen + Werte einfügen / anpassen
-		try {
-			Statement stmt = con.createStatement();
-
-			stmt.executeUpdate("UPDATE element SET name=\"" + e.getName()
-					+ "\", description=\"" + e.getDescription()
-					+ "\", materialdescriptio=\"" + e.getMaterialDescription()
-					+ "\" WHERE id=" + e.getId());
-		} catch (SQLException ex) {
-			throw ex;
-		}
-
-		return e;
 	}
 
 	/**
